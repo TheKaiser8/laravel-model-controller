135d6f9384bdbfad13addb5fd4c14b1c82711b07
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-16 17:18:33 +0100

    Aggiunge stampa cards movies con dettagli Dettagli: titolo, titolo originale, lingua originale, data uscita e voto

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -18,15 +18,27 @@
 <body>
 
     <main>
-        <h1 class="text-center">Homepage</h1>
-        <ul>
-            @foreach ($movies as $movie)
-            <li>
-                {{ $movie->title }}
-            </li>
-                
-            @endforeach
-        </ul>
+        <h1 class="text-center my-3">Movies</h1>
+        <div class="container">
+            <div class="row d-flex gy-5">
+                @foreach ($movies as $movie)
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h5 class="card-title text-center fw-bold">{{ $movie->title }}</h5>
+                                <h6 class="subtitle text-center">{{ $movie->original_title }}</h6>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Lingua orginale: {{ $movie->nationality }}</li>
+                                    <li class="list-group-item">Data uscita: {{ $movie->date }}</li>
+                                    <li class="list-group-item">Voto: {{ $movie->vote }}</li>
+                                  </ul>
+                                
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 
 </body>
