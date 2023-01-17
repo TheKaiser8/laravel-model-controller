74ca3c0989f8bad716ea05d6e7f20a9d3918e543
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-17 14:48:15 +0100

    Aggiunge queries d'esempio in PageController

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,26 @@ class PageController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::all(); // SELECT * FROM movies
+
+        // QUERIES d'esempio:
+        // $movies = Movie::where('nationality', 'american')->get(); // SELECT * FROM movies WHERE nationality = american
+
+        // $movies = Movie::where('vote', '>=', 9)->get(); // SELECT * FROM movies WHERE vote >= 9
+
+        // con 2 o più filtri (chaining dei metodi):
+        // $movies = Movie::where('vote', '>=', 9)->where('nationality', 'american')->get(); // SELECT * FROM movies WHERE vote >= 9 AND WHERE nationality = american
+
+        // OPPURE (andando a capo ad ogni metodo)
+        // $movies = Movie::where('vote', '>=', 9)
+        //                 ->where('nationality', 'american')
+        //                 ->orderBy('title','desc') // ordina i titoli dalla Z alla A
+        //                 ->limit(2) // numero limite di parametri che vogliamo ottenere (2)
+        //                 ->get();
+        // $movies = Movie::where('vote', '>=', 9)
+        //                 ->where('nationality', 'american')
+        //                 ->orderBy('title','desc')
+        //                 ->first(); // NON restituisce la collection ma solamente il primo oggetto (può essere utile nella pagina del prodotto singolo, non nella funzione index)
 
         return view('homepage', compact('movies'));
     }
